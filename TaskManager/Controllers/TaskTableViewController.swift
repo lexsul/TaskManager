2ad7da4ee8231ba6b3529566fb662d540f14758e
Alex Suleymanov <alex@pstu.ru>
2019-06-07 13:30:25 +0500

    Переход между контролерами

--- TaskManager/Controllers/TaskTableViewController.swift
+++ TaskManager/Controllers/TaskTableViewController.swift
@@ -47,14 +47,15 @@ class TaskTableViewController: UITableViewController {
         return cell
     }
     
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let cell = sender as? UITableViewCell {
+            let indexRow = tableView.indexPath(for: cell)?.row
+            let destinationViewController = segue.destination as? TaskInfoViewController
+            destinationViewController?.appModelController = appModelController
+            destinationViewController?.taskId = indexRow ?? 0
+        }
     }
-    */
-
 }
